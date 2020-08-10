d70091ab91b5548dbd405834bdfeb75d14d98468
Xavier Conzet <67985993+the-datadudes@users.noreply.github.com>
2020-08-09 20:01:59 -0500

    Add files via upload

---
+++ myAgeInDogYears.js
@@ -0,0 +1,26 @@
+// Setting my age to a constant variable.
+const myAge = 23;
+
+// Setting the early years in my life to a variable.
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// Assigning a variable to the later years in my life.
+let laterYears = myAge - 2;
+
+// Calculating the later years in my life in dog years
+laterYears *= 4
+
+// Printing both the early years and later years to check my work.
+//console.log(earlyYears)
+//console.log(laterYears)
+
+// Calculating my current age in dog years.
+let myAgeInDogYears = earlyYears + laterYears
+//console.log(myAgeInDogYears)
+
+
+let myName = 'Xavier'.toLowerCase();
+//console.log(myName)
+
+console.log('My name is ' + myName + '. I am ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.' )
